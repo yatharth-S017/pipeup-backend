2f0d6c5acb4966615e52e196657e7a8fdc302a8c
yatharth-S017 <yatharthsingh239@gmail.com>
2026-07-04 15:00:31 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/in/pipeup/backend/PipeupBackendApplication.java
@@ -0,0 +1,13 @@
+package in.pipeup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeupBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/pipeup/backend/PipeupBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pipeup.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeupBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
